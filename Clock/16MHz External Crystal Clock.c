65210356a6a43920d377d0830a2f3e592976b898
CosmaOne <53903510+CosmaOne@users.noreply.github.com>
2020-09-11 15:38:15 +0300

    Update 16MHz External Crystal Clock.c

--- Clock/16MHz External Crystal Clock.c
+++ Clock/16MHz External Crystal Clock.c
@@ -43,15 +43,12 @@ void init()
 	TCCR0 |= (1 << CS02);
 	TIMSK |= (1 << TOIE0);
 	TCNT0 = 0;
-
-	//Set Ports-----------
 	
-	//--LED Display
+	//LED Display
 	DDRB = 0x0F;
 	DDRD = 0xFF;
 	DDRC = 0X05;
 	PORTC = 0X05;
-	//---------------------
 	
 	//Enable Global Interrupts
 	sei();
@@ -71,7 +68,7 @@ ISR(TIMER0_OVF_vect)
 	else
 		i++;
 	
-	PORTD &= 0b11110000; //Turn off all displays.
+	PORTD &= 0xF0; //Turn off all displays.
 	PORTD |= (1 << i); //Light i-th display.
 
 	SevenSegment(digits[i]); //Display digit.
@@ -80,14 +77,17 @@ ISR(TIMER0_OVF_vect)
 ISR(TIMER1_COMPA_vect)
 {
 	clock_second++;
+	
 	if(clock_second >= 60)
 	{
 		clock_second = 0;
 		clock_minute++;
+		
 		if(clock_minute >= 60)
 		{
 			clock_minute = 0;
 			clock_hour++;
+			
 			if(clock_hour >= 10)
 				clock_hour = 0;
 		}
@@ -100,80 +100,83 @@ void SevenSegment(uint8_t n)
 	{
 		switch (n)
 		{
+			//PORTB = XXXXABCD
+			//PORTD = EFG.XXXX
 			case 0:
-			//PORT B=XXXX_ABCD
-			//PORT D=EFG._XXXX
-			PORTB|= 0b00001111;
-			PORTD|=0b11000000;
-			PORTD&=0b11001111;
-			break;
+				PORTB |= 0b00001111;
+				PORTD |= 0b11000000;
+				PORTD &= 0b11001111;
+				break;
 
 			case 1:
-			PORTB|=0b00000110;
-			PORTB&=0b11110110;
-			PORTD&=0b00001111;
-			break;
+				PORTB |= 0b00000110;
+				PORTB &= 0b11110110;
+				PORTD &= 0b00001111;
+				break;
 
 			case 2:
-			PORTB|=0b00001101;
-			PORTB&=0b11111101;
-			PORTD|=0b10100000;
-			PORTD&=0b10101111;
-			break;
+				PORTB |= 0b00001101;
+				PORTB &= 0b11111101;
+				PORTD |= 0b10100000;
+				PORTD &= 0b10101111;
+				break;
 
 			case 3:
-			PORTB|=0b00001111;
-			PORTD|=0b00100000;
-			PORTD&=0b00101111;
-			break;
+				PORTB |= 0b00001111;
+				PORTD |= 0b00100000;
+				PORTD &= 0b00101111;
+				break;
 
 			case 4:
-			PORTB|=0b00000110;
-			PORTB&=0b11110110;
-			PORTD|=0b01100000;
-			PORTD&=0b01101111;
-			break;
+				PORTB |= 0b00000110;
+				PORTB &= 0b11110110;
+				PORTD |= 0b01100000;
+				PORTD &= 0b01101111;
+				break;
 
 			case 5:
-			PORTB|=0b00001011;
-			PORTB&=0b11111011;
-			PORTD|=0b01100000;
-			PORTD&=0b01101111;
-			break;
+				PORTB |= 0b00001011;
+				PORTB &= 0b11111011;
+				PORTD |= 0b01100000;
+				PORTD &= 0b01101111;
+				break;
 
 			case 6:
-			PORTB|=0b00001011;
-			PORTB&=0b11111011;
-			PORTD|=0b11100000;
-			PORTD&=0b11101111;
-			break;
+				PORTB |= 0b00001011;
+				PORTB &= 0b11111011;
+				PORTD |= 0b11100000;
+				PORTD &= 0b11101111;
+				break;
 
 			case 7:
-			PORTB|=0b00001110;
-			PORTB&=0b11111110;
-			PORTD&=0b00001111;
-			break;
+				PORTB |= 0b00001110;
+				PORTB &= 0b11111110;
+				PORTD &= 0b00001111;
+				break;
 
 			case 8:
-			PORTB|=0b00001111;
-			PORTD|=0b11100000;
-			PORTD&=0b11101111;
-			break;
+				PORTB |= 0b00001111;
+				PORTD |= 0b11100000;
+				PORTD &= 0b11101111;
+				break;
 
 			case 9:
-			PORTB|=0b00001111;
-			PORTD|=0b01100000;
-			PORTD&=0b01101111;
-			break;
+				PORTB |= 0b00001111;
+				PORTD |= 0b01100000;
+				PORTD &= 0b01101111;
+				break;
 		}
 	}
 	else
 	{
-		PORTB|=0b00001011;
-		PORTB&=0b11111011;
-		PORTD|=0b11110000;
+		PORTB |= 0b00001011;
+		PORTB &= 0b11111011;
+		PORTD |= 0b11110000;
 	}
-	if(i == 2 && db)PORTD |= (1 << 4);
+	
+	//Light dot.
+	if(i == 2 && db)
+		PORTD |= (1 << 4);
 }
 
 void Print(uint16_t num)
@@ -181,14 +184,19 @@ void Print(uint16_t num)
 	uint8_t i = 0;
 	uint8_t j;
 	
-	if(num > 9999) return;
+	if(num > 9999)
+		return;
+	
 	while(num)
 	{
 		digits[i] = num % 10;
 		i++;
 		num = num / 10;
 	}
-	for(j = i; j < 4; j++) digits[j] = 0; //Clear rest of digits.
-	db = 1; //Light dot.
+	
+	for(j = i; j < 4; j++)
+		digits[j] = 0; //Clear rest of digits.
+	
+	db = 1; //Enable dot.
 }
 
